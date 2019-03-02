75e1909cb4016baf27f1ff2cff30c383d01b0bb5
Arianne Duke <45861081+arianneduke@users.noreply.github.com>
2019-03-02 13:17:27 -0600

    Add files via upload

---
+++ index.hyperesources/trivainfo.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"Weird Trivia";
+	window.questions = [		"Which one of these is NOT a soda Brand?", 
+								"How long is a city block?",
+								"What are the odds of winning the lottery?", 
+								"What’s the active ingredient in most toothpastes?", 
+								"What is the capital city of Arkansas?",
+								"Approximately How many M&M’s come in the standard 1.59 oz bag?",
+								"Which one of these women was not a United States First Lady?",
+								"Which one of these bands cannot be classified as Glam Rock?",
+								"How much caffeine is in a 12oz Can of Pepsi?",
+								"How much does a gallon of milk weigh?" ];
+	window.correctAnswer = [ 	"D", "B", "C", "A", "E", "A", "E", "B", "C", "E" ]; 
+	window.answerA = [ 			"Jones", 
+								"311 Feet", 
+								"89 million to one", 
+								"Fluoride", 
+								"Concord", 
+								"30-35", 
+								"Eleanor Rosalynn Smith", 
+								"Guns N’ Roses", 
+								"15mg", 
+								"2.2 lbs"];
+	window.answerB = [ 			"Crush", 
+								"127 yards", 
+								"45 trillion to one", 
+								"Calcium",
+								"Helena", 
+								"200-250", 
+								"Laura Lane Welch", 
+								"Simon & Garfunkel", 
+								"2705 mg",
+								"7.9 lbs" ];
+	window.answerC = [ 			"Nesbitt’s", 
+								"900 feet", 
+								"175 million to one", 
+								"Oxygen",
+								"Madison", 
+								"15-16", 
+								"Hillary Diane Rodham", 
+								"Kiss", 
+								"39mg",
+								"3.4 lbs" ];
+	window.answerD = [ 			"Duke", 
+								"3000 feet", 
+								"37 to one", 
+								"Methane",
+								"Richmond", 
+								"0-12", 
+								"Michelle LaVaughn Robinson", 
+								"Van Halen", 
+								"3mg",
+								"20 lbs" ];
+	window.answerE = [ 			"Cactus Cooler", 
+								"15 meters", 
+								"25 billion to one", 
+								"Zinc",
+								"Little Rock", 
+								"72-100", 
+								"Amelia Earhart", 
+								"QUEEN", 
+								"0.369mg",
+								"8.6lbs" ];
+	window.response = [ 		"A Duke is historically the highest rank below a monarch.", 
+								"City Blocks are the space for buildings within a city pattern.", 
+								"A lottery is a form of gambling that involves the drawing of numbers at random for a prize.",
+								"Fluorine is estimated to be the 13th most abundant element on earth.",
+								"The Territory of Arkansas was admitted to the Union as the 25th state on June 15, 1836.",
+								"M&M's is the flagship product of the Mars Wrigley Confectionery division of Mars, Incorporated.", 
+								"Earhart was the first female aviator to fly solo across the Atlantic Ocean",
+								"Simon & Garfunkel are classified as folk rock.", 
+								"There are 115-175mg of caffeine in a 7 oz cup of coffee.",
+								"Gallon Smashing gained popularity in 2013-look it up." ];
+							
+}
+						
